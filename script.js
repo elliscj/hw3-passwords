49aedcfc01329b6d756f897e4a6a81ec652496d6
Cam Ellis <elliscj@colorado.edu>
2021-11-06 19:27:49 -0600

    finished writing function and checking for bugs

--- script.js
+++ script.js
@@ -15,17 +15,11 @@ var lowerCase = lowCase.split("");
 var num = "0123456789";
 var numbers = num.split("");
 
-var passArray;
+var passArray = [];
 
 var password = "";
 
 var passwordVariable;
-
-console.log(passArray);
-
-// var randomChar = Math.floor(Math.random() * passArray.length);
-
-// console.log(randomChar);
 
 //
 // Add event listener to generate button
@@ -74,31 +68,51 @@ generateBtn.addEventListener("click", function () {
       ));
   } while (passwordVariable.upperCaseChar === false && passwordVariable.lowerCaseChar === false && passwordVariable.specialCharactersChar === false && passwordVariable.numbersChar === false);
 
-  // if (passwordVariable.upperCaseChar === true) {
-  //   passArray.concat(upperCase);
-  // }
-  // if (passwordVariable.lowerCaseChar === true) {
-  //   passArray.concat(lowerCase);
-  // }
-  // if (passwordVariable.specialCharactersChar === true) {
-  //   passArray.concat(specialCharacters);
-  // }
-  // if (passwordVariable.numbersChar === true) {
-  //   passArray.concat(numbers);
-  // }
+  if (passwordVariable.upperCaseChar === true) {
+    passArray = passArray.concat(upperCase);
+  }
+  if (passwordVariable.lowerCaseChar === true) {
+    passArray = passArray.concat(lowerCase);
+  }
+  if (passwordVariable.specialCharactersChar === true) {
+    passArray = passArray.concat(specialCharacters);
+  }
+  if (passwordVariable.numbersChar === true) {
+    passArray = passArray.concat(numbers);
+  }
   console.log(passwordVariable);
   console.log(passwordVariable.passwordLength);
+
+  console.log(randomChar);
+
+  randomChars = [];
+  //randomChars = [5,9,6,3]
+  //passArray = [A,B,d,h,l,6,8,2]
+  for (i = 0; i < passwordVariable.passwordLength; i++) {
+    var randomChar = Math.floor(Math.random() * passArray.length);
+    randomChars = randomChars.concat(randomChar);
+  }
+
+  for (i = 0; i < randomChars.length; i++) {
+    randomChar = randomChars[i];
+    password = password.concat(passArray[randomChar]);
+  }
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 });
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+generateBtn.addEventListener("click", writePassword);
 
 //do with a loop, loop as many times as password length for each loop grab a random indexed item from the concat array
 
 // console.log(passwordLength);
 
 //parseInt?
-
-// for (i = 0, i < passArray.length, i++ ) {
-
-// }
 
 // //just reprompt and give alert
 
